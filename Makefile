f985aa60ef25d450608df9894365130a9268d6ea
Julio Martins <juliomarttins5@gmail.com>
2025-12-05 20:06:51 -0300

    chore: start initial configuration
    created the initial configuration and add docker compose with watch mode
    resolves #3
    resolves #1

---
+++ Makefile
@@ -0,0 +1,14 @@
+.PHONY: watch watch_down
+
+watch:
+	cat .env* > .env.docker; \
+	docker compose -f ./docker-compose.dev.yaml -p shortener watch --prune; \
+	make watch_down
+
+watch_down:
+	docker compose -f ./docker-compose.dev.yaml -p shortener down shortener --volumes --remove-orphans; \
+	rm -f .env.docker
+
+watch_prune:
+	docker stop $$(docker ps -a -q --filter "label=com.docker.compose.project=shortener"); \
+	docker system prune -a --filter "label=com.docker.compose.project=shortener" -f
---
+++ src/main/java/com/example/urlshortenerapi/config/CassandraConfig.java
@@ -0,0 +1,52 @@
+package com.example.urlshortenerapi.config;
+
+import org.jspecify.annotations.NullMarked;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
+import org.springframework.data.cassandra.core.cql.keyspace.SpecificationBuilder;
+
+import java.util.List;
+
+@Configuration
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.cassandra.contact-points:localhost}")
+    protected String contactPoints;
+
+    @Value("${spring.cassandra.local-datacenter:DC1}")
+    protected String localDatacenter;
+
+    @Value("${spring.cassandra.keyspace:urlshortener}")
+    protected String keyspace;
+
+    @Override
+    @NullMarked
+    protected String getKeyspaceName() {
+        return this.keyspace;
+    }
+
+    @Override
+    protected String getLocalDataCenter() {
+        return this.localDatacenter;
+    }
+
+    @Override
+    @NullMarked
+    protected String getContactPoints() {
+        return this.contactPoints;
+    }
+
+    @Override
+    @NullMarked
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        CreateKeyspaceSpecification specification = SpecificationBuilder.createKeyspace(this.getKeyspaceName())
+                .ifNotExists()
+                .with(KeyspaceOption.DURABLE_WRITES, true)
+                .withSimpleReplication(1);
+
+        return List.of(specification);
+    }
+}
