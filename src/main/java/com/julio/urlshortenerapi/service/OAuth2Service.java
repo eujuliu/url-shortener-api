466e379ee036ff16346756e9964bcffd2b8612f0
Julio Martins <juliomarttins5@gmail.com>
2025-12-19 12:00:59 -0300

    feat: add oauth authentication for login/register

---
+++ src/main/java/com/julio/urlshortenerapi/service/OAuth2Service.java
@@ -0,0 +1,133 @@
+package com.julio.urlshortenerapi.service;
+
+import com.julio.urlshortenerapi.model.OAuthProvider;
+import com.julio.urlshortenerapi.model.User;
+import com.julio.urlshortenerapi.repository.OAuthProviderRepository;
+import com.julio.urlshortenerapi.repository.UserRepository;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OAuth2Service extends DefaultOAuth2UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private OAuthProviderRepository oAuthProviderRepository;
+
+  @Override
+  public OAuth2User loadUser(OAuth2UserRequest request)
+    throws OAuth2AuthenticationException {
+    OAuth2User oAuthUser = super.loadUser(request);
+
+    String registrationId = request.getClientRegistration().getRegistrationId();
+
+    Map<String, Object> attributes = oAuthUser.getAttributes();
+    String name;
+    String email;
+    Boolean emailVerified;
+
+    switch (registrationId) {
+      case "google":
+        name = (String) attributes.get("name");
+        email = (String) attributes.get("email");
+        emailVerified = (Boolean) attributes.get("email_verified");
+        break;
+      case "github":
+        name = (String) attributes.get("name");
+
+        if (name == null) {
+          name = (String) attributes.get("login");
+        }
+
+        email = (String) attributes.get("email");
+        if (email == null) {
+          email = this.fetchGitHubEmail(request);
+        }
+
+        emailVerified = true;
+
+        break;
+      default:
+        throw new OAuth2AuthenticationException(
+          "Unsupported OAuth2 provider: " + registrationId
+        );
+    }
+
+    User user = this.userRepository.findByEmail(email);
+
+    if (user == null) {
+      user = User.builder().name(name).email(email).build();
+
+      this.userRepository.save(user);
+    }
+
+    OAuthProvider oAuthProvider =
+      this.oAuthProviderRepository.findByUserIdAndProvider(
+        user.getUserId(),
+        registrationId
+      );
+
+    if (oAuthProvider == null) {
+      oAuthProvider = OAuthProvider.builder()
+        .userId(user.getUserId())
+        .provider(registrationId)
+        .email(email)
+        .emailVerified(emailVerified != null ? emailVerified : false)
+        .build();
+
+      this.oAuthProviderRepository.save(oAuthProvider);
+    }
+
+    return oAuthUser;
+  }
+
+  private String fetchGitHubEmail(OAuth2UserRequest request) {
+    String emailApiUrl = "https://api.github.com/user/emails";
+    RestTemplate restTemplate = new RestTemplate();
+    HttpHeaders headers = new HttpHeaders();
+
+    headers.setBearerAuth(request.getAccessToken().getTokenValue());
+
+    HttpEntity<String> entity = new HttpEntity<>("", headers);
+
+    try {
+      ResponseEntity<List<Map<String, Object>>> response =
+        restTemplate.exchange(
+          emailApiUrl,
+          HttpMethod.GET,
+          entity,
+          new ParameterizedTypeReference<List<Map<String, Object>>>() {}
+        );
+
+      List<Map<String, Object>> emails = response.getBody();
+
+      if (emails != null) {
+        return emails
+          .stream()
+          .filter(e -> (Boolean) e.get("primary"))
+          .map(e -> (String) e.get("email"))
+          .findFirst()
+          .orElse(null);
+      }
+    } catch (Exception e) {
+      throw new OAuth2AuthenticationException(
+        "Error getting Github primary email"
+      );
+    }
+    return null;
+  }
+}
--- src/main/java/com/julio/urlshortenerapi/shared/config/SecurityConfig.java
+++ src/main/java/com/julio/urlshortenerapi/shared/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.julio.urlshortenerapi.shared.config;
 
+import com.julio.urlshortenerapi.service.OAuth2Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+  @Autowired
+  private OAuth2Service oauth2Service;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -18,11 +28,42 @@ public class SecurityConfig {
   }
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http)
-    throws Exception {
-    http.csrf(csrf -> csrf.disable());
+  public SecurityFilterChain securityFilterChain(
+    HttpSecurity http,
+    @Value("${app.security.oauth2.success-url}") String successUrl,
+    @Value("${app.security.oauth2.failure-url}") String failureUrl
+  ) throws Exception {
+    http.csrf(csrf ->
+      csrf
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+        .ignoringRequestMatchers("/api/v1/user")
+    );
+
     http.authorizeHttpRequests(auth -> {
-      auth.requestMatchers("/**").permitAll();
+      auth.requestMatchers(HttpMethod.POST, "/api/v1/user").permitAll();
+      auth.requestMatchers("/actuator/**", "/api/v1/login/**").permitAll();
+      auth.anyRequest().authenticated();
+    });
+
+    http.oauth2Login(oauth2 -> {
+      oauth2.authorizationEndpoint(auth -> {
+        auth.baseUri("/api/v1/login/oauth2/authorization");
+      });
+      oauth2.redirectionEndpoint(redirection ->
+        redirection.baseUri("/api/v1/login/oauth2/code/*")
+      );
+
+      oauth2.defaultSuccessUrl(successUrl, true);
+      oauth2.failureUrl(failureUrl);
+
+      oauth2.userInfoEndpoint(info -> {
+        info.userService(this.oauth2Service);
+      });
+    });
+
+    http.exceptionHandling(exceptions -> {
+      exceptions.authenticationEntryPoint(new Http403ForbiddenEntryPoint());
     });
 
     return http.build();
